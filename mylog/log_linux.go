6ecd63512e84e9b72852195fe3054703c4228032
onedss <onedss@qq.com>
2022-11-24 15:38:00 +0800

    Fix: 修改在Linux下编译的BUG。

--- mylog/log_linux.go
+++ mylog/log_linux.go
@@ -1,7 +1,10 @@
 package mylog
 
 import (
+	"fmt"
 	"os"
+	"path/filepath"
+	"strings"
 	"syscall"
 )
 
@@ -16,4 +19,40 @@ func RedirectStderr() (err error) {
 		return
 	}
 	return
+}
+
+func ErrorLogFilename() string {
+	return filepath.Join(LogDir(), fmt.Sprintf("%s-error.log", strings.ToLower(EXEName())))
+}
+
+func LogDir() string {
+	dir := filepath.Join(CWD(), "logs")
+	EnsureDir(dir)
+	return dir
+}
+
+func EXEName() string {
+	path, err := os.Executable()
+	if err != nil {
+		return ""
+	}
+	return strings.TrimSuffix(filepath.Base(path), filepath.Ext(path))
+}
+
+func EnsureDir(dir string) (err error) {
+	if _, err = os.Stat(dir); os.IsNotExist(err) {
+		err = os.MkdirAll(dir, 0755)
+		if err != nil {
+			return
+		}
+	}
+	return
+}
+
+func CWD() string {
+	path, err := os.Executable()
+	if err != nil {
+		return ""
+	}
+	return filepath.Dir(path)
 }
