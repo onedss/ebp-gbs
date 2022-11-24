c43b72b3b19f6eab57633acc7de7c1e4716cec96
onedss <onedss@qq.com>
2022-11-24 15:41:38 +0800

    Fix:

--- mylog/log_linux.go
+++ mylog/log_linux.go
@@ -1,16 +1,14 @@
 package mylog
 
 import (
-	"fmt"
+	"github.com/onedss/ebp-gbs/utils"
 	"os"
-	"path/filepath"
-	"strings"
 	"syscall"
 )
 
 // RedirectStderr to the file passed in
 func RedirectStderr() (err error) {
-	logFile, err := os.OpenFile(ErrorLogFilename(), os.O_WRONLY|os.O_CREATE|os.O_SYNC|os.O_APPEND, 0644)
+	logFile, err := os.OpenFile(utils.ErrorLogFilename(), os.O_WRONLY|os.O_CREATE|os.O_SYNC|os.O_APPEND, 0644)
 	if err != nil {
 		return
 	}
@@ -19,40 +17,4 @@ func RedirectStderr() (err error) {
 		return
 	}
 	return
-}
-
-func ErrorLogFilename() string {
-	return filepath.Join(LogDir(), fmt.Sprintf("%s-error.log", strings.ToLower(EXEName())))
-}
-
-func LogDir() string {
-	dir := filepath.Join(CWD(), "logs")
-	EnsureDir(dir)
-	return dir
-}
-
-func EXEName() string {
-	path, err := os.Executable()
-	if err != nil {
-		return ""
-	}
-	return strings.TrimSuffix(filepath.Base(path), filepath.Ext(path))
-}
-
-func EnsureDir(dir string) (err error) {
-	if _, err = os.Stat(dir); os.IsNotExist(err) {
-		err = os.MkdirAll(dir, 0755)
-		if err != nil {
-			return
-		}
-	}
-	return
-}
-
-func CWD() string {
-	path, err := os.Executable()
-	if err != nil {
-		return ""
-	}
-	return filepath.Dir(path)
 }
